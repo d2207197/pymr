9cc75948f8bd5a478ffc404b8e13b88e6e17facf
顏孜羲 <joseph.yen@gmail.com>
2015-06-12 22:31:21 +0800

    sub command style.

--- pymr/pymr.py
+++ pymr/pymr.py
@@ -11,8 +11,9 @@ import os
 from six.moves import map
 from itertools import groupby
 import six
+import pickle
 
-MAPPER, REDUCER = sys.argv[0] + ' -m', sys.argv[0] + ' -r',
+MAPPER, REDUCER = sys.argv[0] + ' map', sys.argv[0] + ' reduce',
 
 
 class PyMR(object):
@@ -57,23 +58,36 @@ class PyMR(object):
     @staticmethod
     def _argparser(description):
         parser = argparse.ArgumentParser(description=description)
-        mode_group = parser.add_mutually_exclusive_group(required=True)
-        mode_group.add_argument(
-            '-H', '--hadoop-streaming', nargs=2, metavar=('INPUT', 'OUTPUT'), help='run in Hadoop mode. (INPUT: a folder, file in HDFS)')
-        mode_group.add_argument(
-            '-L', '--local-mr', nargs=2, metavar=('INPUT', 'OUTPUT'), help='run in local mapreduce mode. (INPUT: a folder, file or `-\'(stdin))')
-        mode_group.add_argument(
-            '-P', '--local-pipe', nargs='*', metavar='FILE', help='run in pipeline mode')
-        mode_group.add_argument(
-            '-r', '--reducer', nargs='*', metavar='FILE', help='Reducer mode')
-        mode_group.add_argument(
-            '-m', '--mapper', nargs='*', metavar='FILE', help='Mapper mode')
-        parser.add_argument(
-            '--num-reducer', metavar='N', type=int, help='number of reducer', default=4)
-        parser.add_argument('--split-size', metavar='SIZE', help='size of splits', default='10m')
-        parser.add_argument('--lmr-cmd', metavar='LMR', help='lmr command', default='lmr')
-        parser.add_argument('--hadoop-streaming-jar', metavar='PATH',
-                            help='hadoop streaming jar path', default='/usr/lib/hadoop-mapreduce/hadoop-streaming.jar')
+        subparsers = parser.add_subparsers(help='sub-commands', dest='cmd')
+        parser_hstreaming = subparsers.add_parser('hadoop', help='Hadoop Streaming mode')
+        parser_localmr = subparsers.add_parser('lmr', help='local mapreduce mode')
+        parser_pipe = subparsers.add_parser('pipe', help='pipeline mode')
+        parser_map = subparsers.add_parser('map', help='mapper mode')
+        parser_reducer = subparsers.add_parser('reduce', help='reducer mode')
+        
+        parser_hstreaming.add_argument('INPUT', help='input folder/file in HDFS')
+        parser_hstreaming.add_argument('OUTPUT', help='output path in HDFS')
+        parser_hstreaming.add_argument('-j', '--hadoop-streaming-jar', metavar='PATH_TO_JAR',
+                            help='[HSTREAMING] hadoop streaming jar path. (default: %(default)s)',
+default='/usr/lib/hadoop-mapreduce/hadoop-streaming.jar')
+        parser_hstreaming.add_argument('-n',
+            '--num-reducer', metavar='N', type=int, help='[LMR/HSTREAMING]number of reducer. (default: %(default)s)', default=4)
+
+                                       
+        parser_localmr.add_argument('INPUT', help='input folder/file HDFS. `-\' for stdin')
+        parser_localmr.add_argument('OUTPUT', help='output path')
+        parser_localmr.add_argument('-c', '--lmr-cmd', metavar='LMR_CMD', help='[LMR] lmr command. (default: %(default)s)', default='lmr')
+        parser_localmr.add_argument('-s','--split-size', metavar='SIZE', help='[LMR] size of splits. (default: %(default)s)', default='10m')
+
+        parser_localmr.add_argument('-n',
+            '--num-reducer', metavar='N', type=int, help='[LMR/HSTREAMING]number of reducer. (default: %(default)s)', default=4)
+
+        parser_pipe.add_argument('FILE', nargs = '*', help='input files')
+
+        parser_map.add_argument('FILE', nargs = '*', help='input files')
+        parser_reducer.add_argument('FILE', nargs = '*', help='input files')
+
+        
         # parser.add_argument('--mapper-argument', help='argument for mapper') # TODO
         # parser.add_argument('--reducer-argument', help='argument for reducer') # TODO
         return parser.parse_args()
@@ -85,29 +99,29 @@ class PyMR(object):
     def run(self):
         args = PyMR._argparser(self.description)
         # print(args, file= sys.stderr)
-        if args.mapper is not None:
-            mapper_out = self._do_mapper(args.mapper)
+        if args.cmd == 'map':
+            mapper_out = self._do_mapper(args.FILE)
             # mapper_out = do_combiner(mapper_out)
             for key, value in mapper_out:
                 print('{}\t{}'.format(key, value))
-        elif args.reducer is not None:
-            reducer_out = self._do_reducer(args.reducer)
+                
+        elif args.cmd == 'reduce':
+            reducer_out = self._do_reducer(args.FILE)
             for key, value in reducer_out:
                 print('{}\t{}'.format(key, value))
-        elif args.localpipe is not None:
-            input_files = args.localpipe
-            _localpipe(input_files)
-        elif args.localmr is not None:
-            input, output = args.localmr
-            _localmr(input, output, args.lmr_cmd, args.split_size, args.num_reducer)
-        elif args.hadoop_streaming is not None:
-            input, output = args.hadoop_streaming
-            _hstreaming(input, output, args.num_reducer, args.hadoop_streaming_jar, self._files)
+                
+        elif args.cmd == 'pipe':
+            _localpipe(args.FILE)
+            
+        elif args.cmd == 'lmr':
+            _localmr(args.INPUT, args.OUTPUT, args.lmr_cmd, args.split_size, args.num_reducer)
+            
+        elif args.cmd == 'hadoop' :
+            _hstreaming(args.INPUT, args.OUTPUT, args.num_reducer, args.hadoop_streaming_jar, self._files)
 
 
 def _get_input_process(input):
     if input == '-':
-        print('-')
         return sys.stdin
     input = os.path.normpath(input)
     assert os.access(input, os.R_OK)
@@ -119,19 +133,13 @@ def _get_input_process(input):
 
     return subprocess.Popen(['cat'] + input_files, stdout=subprocess.PIPE).stdout
 
-    # return 'pv {}'.format(input)
-    # else:
-    # raise Exception('Input is not file nor directory: {}'.format(input))
-
 
 def _localmr(input, output, lmr_cmd, split_size, num_reducer):
-    # input_files = _input_files(input)
-    # print(input_cmd)
+    
     input_data = _get_input_process(input)
     lmr_process = subprocess.Popen(
         [lmr_cmd, split_size, str(num_reducer), MAPPER, REDUCER, output], stdin=input_data)
-    # lmr_process = subprocess.Popen(['cat'], stdin=input_process.stdout)
-    # # input_process.stdout.close()
+    input_data.close()
     lmr_process.wait()
 
 
