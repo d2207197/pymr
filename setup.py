7eb3ce72e1d1687fd098e4a932dbb571ac93a6f5
顏孜羲 <joseph.yen@gmail.com>
2015-08-09 22:34:31 +0800

    require six

--- setup.py
+++ setup.py
@@ -59,12 +59,12 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    packages=['pymr']
+    packages=['pymr'],
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=['peppercorn'],
+    install_requires=['six'],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
